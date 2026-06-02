447b02f715a94d869ae709818aa8e23933151292
Henya Katz <Hc216307900@sgur.org.il>
2026-06-02 17:16:37 +0300

    בדיקות פייפליין

--- server/eslint.config.mjs
+++ server/eslint.config.mjs
@@ -1,19 +1,27 @@
-import js from "@eslint/js";
-import globals from "globals";
-
+// eslint.config.mjs
 export default [
-
-  js.configs.recommended,
   {
-    parserOptions: {
-      sourceType: "module",
-    },
+    // כללי ל־JavaScript ב־Node
+    files: ["**/*.js"],
+
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "commonjs",
-      globals: {
-        ...globals.node,
+      parserOptions: {
+        ecmaVersion: "latest", // גרסת JS עדכנית
+        sourceType: "module",  // מאפשר import/export
       },
+    },
+
+    env: {
+      node: true,
+      es2021: true,
+    },
+
+    // כללים בסיסיים מומלצים
+    rules: {
+      "no-unused-vars": ["error", { "argsIgnorePattern": "^_" }], // _ignored args
+      "no-console": "off", // אפשר console.log בשרת
+      "semi": ["error", "always"], // סמי-קולון חובה
+      "quotes": ["error", "double"], // ציטוטים כפולים
     },
   },
 ];
