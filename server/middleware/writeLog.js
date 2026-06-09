2f62445fc5e2970038b5fe8337f14d6aed8fc32a
Henya Katz <Hc216307900@sgur.org.il>
2026-06-09 12:33:35 +0300

    הרצת פיפליין

--- server/middleware/writeLog.js
+++ server/middleware/writeLog.js
@@ -4,6 +4,8 @@ const path = require('path');
 const requestsLogFile = path.join(__dirname, '../logs.log');
 const errorsLogFile = path.join(__dirname, '../logs.log');
 
+
+
 function writeLog(message, type = 'request') {
   const file = type === 'error' ? errorsLogFile : requestsLogFile;
   fs.appendFile(file, message, (err) => { if(err) console.error(err) })
